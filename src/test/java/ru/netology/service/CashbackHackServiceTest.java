9ff89dfead958bd725e6dc33671c3638bd97ba9d
MarniaTu <147163020+MarniaTu@users.noreply.github.com>
2024-02-01 09:42:45 +0300

    add tests using TestNG

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,61 @@
+package ru.netology.service;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateAdditionalPurchaseAmountIfAmount900() {
+        CashbackHackService service = new CashbackHackService();
+
+        service.remain(900);
+
+        assertEquals(service.remain(900), 100);
+
+    }
+
+    @Test
+    public void shouldCalculateAdditionalPurchaseAmountIfAmount999() {
+        CashbackHackService service = new CashbackHackService();
+
+        service.remain(999);
+
+        assertEquals(service.remain(999), 1);
+
+    }
+
+    @Test
+    public void shouldNotCalculateAdditionalPurchaseAmountIfAmount1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        service.remain(1000);
+
+        assertEquals(service.remain(1000), 0);
+
+    }
+
+    @Test
+    public void shouldCalculateAdditionalPurchaseAmountIfAmount1001() {
+        CashbackHackService service = new CashbackHackService();
+
+        service.remain(1001);
+
+        assertEquals(service.remain(1001), 999);
+
+    }
+
+    @Test
+    public void shouldCalculateAdditionalPurchaseAmountIfAmountZero() {
+        CashbackHackService service = new CashbackHackService();
+
+        service.remain(0);
+
+        assertEquals(service.remain(0), 1000);
+
+    }
+
+}
+
